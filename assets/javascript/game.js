e86c87b3cc64dcf0fbe3d92b18104c76c09c0162
Terence Ro <terencero@dhcp-10-102-236-249.wireless.northwestern.private>
2016-10-24 14:05:28 -0500

    code html skeleton, link reset and empty style css, typed intial comments for jQuery build/plan

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+// global variables
+
+// stats variables (in object)
+// hp var
+// attack points var
+// counter attack points var
+
+// stats on html var
+// hp
+// attack
+// counter attack
+
+// character image variables
+// img 1
+// img 2
+// img 3
+// img 4
+
+// image divs on html
+// character selection (will hold "your character image") div
+// opponent container div
+// defender character div
+
+// function to set game board
+	//set intial stats (hp points)
+
+// function to move character images in correct divs
+	// if character chosen, then move other characters to opponent container
+
+// function to assign initial attack points for character selected and consistent counter attack points
+	// if character chosen, assign initial attack points, else assign counterattack points
+		// function for game stats and progress
+			// build in attack iterator to double after each attack i+2
+			// build in counter attack iterator to attack with same points without incrementing
+			// build in main character hp decrementer to carry over to subsequent rounds
+			// build in opponent hp decrementer
+		// function if defender defeated remove defender image from defender container			
+
+// function to alert win or loose
+	// if no more opponents, the alert a congratulations, else play again option
+
+
