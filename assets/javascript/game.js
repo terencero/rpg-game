4529d792ab8beaf8bf67236066dd8b203efe8bfe
Terence Ro <terencero@Terences-MacBook-Air.local>
2016-10-26 23:34:51 -0500

    move character divs to different section.  appendTo() works append() does not

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
 // global variables
 
 // stats variables and image url (in object): hp var, attack points var, counter attack points var
-var char1 = {name: "Yoda", image: src="#", hp: 150, attack: 20};
-var char2 = { name: "Obi Wan", image: src="#", hp: 150, attack: 20 };
-var char3 = { name: "Darth Sidious", image: src="#", hp: 150, attack: 20 };
-var char4 = { name: "Darth Vader", image: src="#", hp: 150, attack: 20 };
+var char1 = {name: "Yoda", image: src="#", hp: 180, attack: 20, counterattack: 10 };
+var char2 = { name: "Obi Wan", image: src="#", hp: 150, attack: 20, counterattack: 10 };
+var char3 = { name: "Darth Sidious", image: src="#", hp: 170, attack: 20, counterattack: 10 };
+var char4 = { name: "Darth Vader", image: src="#", hp: 130, attack: 20, counterattack: 10 };
 
 // stats on html var
 // hp
@@ -57,6 +57,25 @@ $.each (char4, function(key, value){
 
 // function to assign initial attack points for character selected and consistent counter attack points
 	// if character chosen, assign initial attack points, else assign counterattack points
+	function chooseCharacter() {
+		$('.character').click(function(event){
+			console.log('clicked');
+			// if(char1){
+			// 	$('.opponent-container').append('#char2');
+			// 	$('.opponent-container').append('#char3');
+			// 	$('.opponent-container').append('#char4');
+			// }
+			});
+		$('#char1').click(function(event){
+			console.log('clicked');
+				$('.opponent-container').append('#c2');
+				$('#c3').appendTo('.opponent-container');
+				$('.opponent-container').append('#char4');
+			});
+		}
+	
+
+	chooseCharacter();
 		// function for game stats and progress
 			// build in attack iterator to double after each attack i+2
 			// build in counter attack iterator to attack with same points without incrementing
